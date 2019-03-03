f01ddae09aeb39def03f21ded61a6c9a18770e66
Waterstrong <sqlin@thoughtworks.com>
2019-03-03 15:41:00 +1100

    extract getBuzz and replace FizzBuzz

--- src/main/java/au/com/workshop/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/au/com/workshop/tdd/fizzbuzz/FizzBuzz.java
@@ -1,35 +1,24 @@
 package au.com.workshop.tdd.fizzbuzz;
 
-import org.apache.commons.lang3.StringUtils;
-
 import static java.lang.String.valueOf;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
 
 public class FizzBuzz {
-
     private static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
-    private static final String FIZZ_BUZZ = FIZZ + BUZZ;
     private static final int THREE = 3;
     private static final int FIVE = 5;
 
     public String calculate(int number) {
-        String result;
-
-        result = getFizz(number);
-
-        if (number % FIVE == 0) {
-            result = BUZZ;
-        }
-
-        if (number % 3 == 0 && number % 5 == 0) {
-            result = FIZZ_BUZZ;
-        }
-
-        return StringUtils.defaultIfEmpty(result, valueOf(number));
+        return defaultIfEmpty(getFizz(number) + getBuzz(number), valueOf(number));
     }
 
     private String getFizz(int number) {
         return number % THREE == 0 ? FIZZ : EMPTY;
+    }
+
+    private String getBuzz(int number) {
+        return number % FIVE == 0 ? BUZZ : EMPTY;
     }
 }
