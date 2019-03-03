6b0ef0978d896a5266a8c8e5ad46fa6263473b5a
Waterstrong <sqlin@thoughtworks.com>
2019-03-03 15:25:51 +1100

    return number itself case

--- src/main/java/au/com/workshop/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/au/com/workshop/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,10 @@
 package au.com.workshop.tdd.fizzbuzz;
 
+import static java.lang.String.valueOf;
+
 public class FizzBuzz {
+    public String calculate(int number) {
+
+        return valueOf(number);
+    }
 }
--- src/test/java/au/com/workshop/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/au/com/workshop/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,5 +1,17 @@
 package au.com.workshop.tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
 
+    private final FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    void shouldReturnItselfGivenNumberCannotBeDivedByThreeOrFive() {
+        assertEquals("1", fizzBuzz.calculate(1));
+        assertEquals("2", fizzBuzz.calculate(2));
+        assertEquals("11", fizzBuzz.calculate(11));
+    }
 }
