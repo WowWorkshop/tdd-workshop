243cd318726199c7d22ce7a4910e44c2ff690128
Waterstrong <sqlin@thoughtworks.com>
2019-04-29 21:10:12 +1000

    Shuiqiang - remove FizzBuzz code

--- src/main/java/au/com/workshop/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/au/com/workshop/tdd/fizzbuzz/FizzBuzz.java
@@ -1,27 +1,4 @@
 package au.com.workshop.tdd.fizzbuzz;
 
-import static java.lang.String.valueOf;
-import static org.apache.commons.lang3.StringUtils.EMPTY;
-import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
-
 public class FizzBuzz {
-    private static final String FIZZ = "Fizz";
-    private static final String BUZZ = "Buzz";
-    private static final Integer THREE = 3;
-    private static final Integer FIVE = 5;
-
-    public String calculate(int number) {
-        if (number <= 0) {
-            throw new InvalidException();
-        }
-        return defaultIfEmpty(getFizz(number) + getBuzz(number), valueOf(number));
-    }
-
-    private String getFizz(int number) {
-        return number % THREE == 0 || valueOf(number).contains(THREE.toString()) ? FIZZ : EMPTY;
-    }
-
-    private String getBuzz(int number) {
-        return number % FIVE == 0 || valueOf(number).contains(FIVE.toString()) ? BUZZ : EMPTY;
-    }
 }
